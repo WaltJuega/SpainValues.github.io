c822991a375fbb614aea64a8a0ac4275274baaa4
WaltJuega <102927076+WaltJuega@users.noreply.github.com>
2022-08-11 11:41:26 +0200

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -1,461 +1,254 @@
 ideologies = [
     {
-        "name": "Anarcho-Communism",
+        "name": "Federación Anarquista Ibérica",
         "stats": {
             "econ": 100,
-            "dipl": 50,
+            "dipl": 85,
             "govt": 100,
+            "scty": 75
+        }
+    },
+    {
+        "name": "Partido Comunista de España",
+        "stats": {
+            "econ": 100,
+            "dipl": 80,
+            "govt": 80,
+            "scty": 85
+        }
+    },
+    {
+        "name": "Partido Comunista de los Pueblos de España",
+        "stats": {
+            "econ": 100,
+            "dipl": 70,
+            "govt": 80,
+            "scty": 75
+        }
+    },
+    {
+        "name": "Partido Obrero de Unificación Marxista",
+        "stats": {
+            "econ": 100,
+            "dipl": 70,
+            "govt": 80,
+            "scty": 70
+        }
+    },
+    {
+        "name": "Frente Obrero",
+        "stats": {
+            "econ": 100,
+            "dipl": 80,
+            "govt": 85,
+            "scty": 70
+        }
+    },
+    {
+        "name": "Frente Popular",
+        "stats": {
+            "econ": 100,
+            "dipl": 90,
+            "govt": 85,
+            "scty": 75
+        }
+    },
+    {
+        "name": "Partido Comunista de España (ML)",
+        "stats": {
+            "econ": 100,
+            "dipl": 40,
+            "govt": 80,
+            "scty": 65
+        }
+    },
+    {
+        "name": "Izquierda Radical Socialista",
+        "stats": {
+            "econ": 85,
+            "dipl": 65,
+            "govt": 85,
+            "scty": 75
+        }
+    },
+    {
+        "name": "Mas Pais",
+        "stats": {
+            "econ": 85,
+            "dipl": 80,
+            "govt": 85,
+            "scty": 70
+        }
+    },
+    {
+        "name": "Segunda Republica Española",
+        "stats": {
+            "econ": 80,
+            "dipl": 85,
+            "govt": 90,
             "scty": 90
         }
     },
     {
-        "name": "Libertarian Communism",
+        "name": "Partido Marxista-Leninista",
         "stats": {
             "econ": 100,
+            "dipl": 35,
+            "govt": 80,
+            "scty": 75
+        }
+    },
+    {
+        "name": "Partido Socialista Unificado",
+        "stats": {
+            "econ": 90,
+            "dipl": 75,
+            "govt": 80,
+            "scty": 75
+        }
+    },
+    {
+        "name": "Izquierda Unida",
+        "stats": {
+            "econ": 85,
+            "dipl": 80,
+            "govt": 85,
+            "scty": 80
+        }
+    },
+    {
+        "name": "Partido de Acción Socialista",
+        "stats": {
+            "econ": 80,
             "dipl": 70,
             "govt": 80,
-            "scty": 80
+            "scty": 75
         }
     },
     {
-        "name": "Trotskyism",
-        "stats": {
-            "econ": 100,
-            "dipl": 100,
-            "govt": 60,
-            "scty": 80
-        }
-    },
-    {
-        "name": "Marxism",
-        "stats": {
-            "econ": 100,
-            "dipl": 70,
-            "govt": 40,
-            "scty": 80
-        }
-    },
-    {
-        "name": "De Leonism",
-        "stats": {
-            "econ": 100,
-            "dipl": 30,
-            "govt": 30,
-            "scty": 80
-        }
-    },
-    {
-        "name": "Leninism",
-        "stats": {
-            "econ": 100,
-            "dipl": 40,
-            "govt": 20,
-            "scty": 70
-        }
-    },
-    {
-        "name": "Stalinism/Maoism",
-        "stats": {
-            "econ": 100,
-            "dipl": 20,
-            "govt": 0,
-            "scty": 60
-        }
-    },
-    {
-        "name": "Religious Communism",
-        "stats": {
-            "econ": 100,
-            "dipl": 50,
-            "govt": 30,
-            "scty": 30
-        }
-    },
-    {
-        "name": "State Socialism",
+        "name": "Confederación Nacional del Trabajo",
         "stats": {
             "econ": 80,
-            "dipl": 30,
-            "govt": 30,
-            "scty": 70
+            "dipl": 90,
+            "govt": 100,
+            "scty": 8
         }
     },
     {
-        "name": "Theocratic Socialism",
+        "name": " Unidas Podemos",
         "stats": {
-            "econ": 80,
-            "dipl": 50,
-            "govt": 30,
-            "scty": 20
+            "econ": 75,
+            "dipl": 80,
+            "govt": 80,
+            "scty": 85
         }
     },
     {
-        "name": "Religious Socialism",
+        "name": "PSOE",
         "stats": {
-            "econ": 80,
-            "dipl": 50,
+            "econ": 70,
+            "dipl": 80,
             "govt": 70,
-            "scty": 20
-        }
-    },
-    {
-        "name": "Democratic Socialism",
-        "stats": {
-            "econ": 80,
-            "dipl": 50,
-            "govt": 50,
             "scty": 80
         }
     },
     {
-        "name": "Revolutionary Socialism",
+        "name": "Partido Popular",
         "stats": {
-            "econ": 80,
-            "dipl": 20,
-            "govt": 50,
-            "scty": 70
-        }
-    },
-    {
-        "name": "Libertarian Socialism",
-        "stats": {
-            "econ": 80,
-            "dipl": 80,
-            "govt": 80,
-            "scty": 80
-        }
-    },
-    {
-        "name": "Anarcho-Syndicalism",
-        "stats": {
-            "econ": 80,
-            "dipl": 50,
-            "govt": 100,
-            "scty": 80
-        }
-    },
-    {
-        "name": "Left-Wing Populism",
-        "stats": {
-            "econ": 60,
-            "dipl": 40,
-            "govt": 30,
-            "scty": 70
-        }
-    },
-    {
-        "name": "Theocratic Distributism",
-        "stats": {
-            "econ": 60,
-            "dipl": 40,
-            "govt": 30,
-            "scty": 20
-        }
-    },
-    {
-        "name": "Distributism",
-        "stats": {
-            "econ": 60,
-            "dipl": 50,
-            "govt": 50,
-            "scty": 20
-        }
-    },
-    {
-        "name": "Social Liberalism",
-        "stats": {
-            "econ": 60,
-            "dipl": 60,
-            "govt": 60,
-            "scty": 80
-        }
-    },
-    {
-        "name": "Christian Democracy",
-        "stats": {
-            "econ": 60,
-            "dipl": 60,
-            "govt": 40,
-            "scty": 30
-        }
-    },
-    {
-        "name": "Social Democracy",
-        "stats": {
-            "econ": 60,
+            "econ": 30,
             "dipl": 70,
-            "govt": 40,
-            "scty": 80
+            "govt": 70,
+            "scty": 30
         }
     },
     {
-        "name": "Progressivism",
+        "name": "Unión General de Trabajadores",
         "stats": {
-            "econ": 60,
+            "econ": 80,
+            "dipl": 90,
+            "govt": 85,
+            "scty": 75
+        }
+    },
+    {
+        "name": "Movimiento Mutualista",
+        "stats": {
+            "econ": 75,
             "dipl": 80,
-            "govt": 60,
-            "scty": 100
-        }
-    },
-    {
-        "name": "Anarcho-Mutualism",
-        "stats": {
-            "econ": 60,
-            "dipl": 50,
             "govt": 100,
-            "scty": 70
+            "scty": 75
         }
     },
     {
-        "name": "National Totalitarianism",
+        "name": "Franquismo",
         "stats": {
             "econ": 50,
             "dipl": 20,
-            "govt": 0,
-            "scty": 50
-        }
-    },
-    {
-        "name": "Global Totalitarianism",
-        "stats": {
-            "econ": 50,
-            "dipl": 80,
-            "govt": 0,
-            "scty": 50
-        }
-    },
-    {
-        "name": "Technocracy",
-        "stats": {
-            "econ": 60,
-            "dipl": 60,
-            "govt": 20,
-            "scty": 70
-        }
-    },
-    {
-        "name": "Centrist",
-        "stats": {
-            "econ": 50,
-            "dipl": 50,
             "govt": 50,
-            "scty": 50
-        }
-    },
-    {
-        "name": "Liberalism",
-        "stats": {
-            "econ": 50,
-            "dipl": 60,
-            "govt": 60,
-            "scty": 60
-        }
-    },
-    {
-        "name": "Religious Anarchism",
-        "stats": {
-            "econ": 50,
-            "dipl": 50,
-            "govt": 0,
-            "scty": 20
-        }
-    },
-    {
-        "name": "Right-Wing Populism",
-        "stats": {
-            "econ": 40,
-            "dipl": 30,
-            "govt": 30,
-            "scty": 30
-        }
-    },
-    {
-        "name": "Moderate Conservatism",
-        "stats": {
-            "econ": 40,
-            "dipl": 40,
-            "govt": 50,
-            "scty": 30
-        }
-    },
-    {
-        "name": "Reactionary",
-        "stats": {
-            "econ": 40,
-            "dipl": 40,
-            "govt": 40,
             "scty": 10
         }
     },
     {
-        "name": "Social Libertarianism",
+        "name": "Falange Española",
         "stats": {
             "econ": 60,
-            "dipl": 70,
-            "govt": 80,
+            "dipl": 15,
+            "govt": 50,
+            "scty": 15
+        }
+    },
+    {
+        "name": "VOX",
+        "stats": {
+            "econ": 50,
+            "dipl": 40,
+            "govt": 50,
+            "scty": 20
+        }
+    },
+    {
+        "name": "Apolitico",
+        "stats": {
+            "econ": 50,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Ciudadanos",
+        "stats": {
+            "econ": 65,
+            "dipl": 80,
+            "govt": 70,
             "scty": 70
         }
     },
     {
-        "name": "Libertarianism",
-        "stats": {
-            "econ": 40,
-            "dipl": 60,
-            "govt": 80,
-            "scty": 60
-        }
-    },
-    {
-        "name": "Anarcho-Egoism",
-        "stats": {
-            "econ": 40,
-            "dipl": 50,
-            "govt": 100,
-            "scty": 50
-        }
-    },
-    {
-        "name": "Nazism",
-        "stats": {
-            "econ": 40,
-            "dipl": 0,
-            "govt": 0,
-            "scty": 5
-        }
-    },
-    {
-        "name": "Autocracy",
+        "name": "Juntas Españolas",
         "stats": {
             "econ": 50,
-            "dipl": 20,
-            "govt": 20,
-            "scty": 50
-        }
-    },
-    {
-        "name": "Fascism",
-        "stats": {
-            "econ": 40,
-            "dipl": 20,
-            "govt": 20,
-            "scty": 20
-        }
-    },
-    {
-        "name": "Capitalist Fascism",
-        "stats": {
-            "econ": 20,
-            "dipl": 20,
-            "govt": 20,
-            "scty": 20
-        }
-    },
-    {
-        "name": "Conservatism",
-        "stats": {
-            "econ": 30,
-            "dipl": 40,
-            "govt": 40,
-            "scty": 20
-        }
-    },
-    {
-        "name": "Neo-Liberalism",
-        "stats": {
-            "econ": 30,
-            "dipl": 30,
-            "govt": 50,
-            "scty": 60
-        }
-    },
-    {
-        "name": "Classical Liberalism",
-        "stats": {
-            "econ": 30,
-            "dipl": 60,
-            "govt": 60,
-            "scty": 80
-        }
-    },
-    {
-        "name": "Authoritarian Capitalism",
-        "stats": {
-            "econ": 20,
-            "dipl": 30,
-            "govt": 20,
-            "scty": 40
-        }
-    },
-    {
-        "name": "State Capitalism",
-        "stats": {
-            "econ": 20,
-            "dipl": 50,
+            "dipl": 15,
             "govt": 30,
-            "scty": 50
+            "scty": 15
         }
     },
     {
-        "name": "Neo-Conservatism",
+        "name": "Carlismo
         "stats": {
-            "econ": 20,
-            "dipl": 20,
-            "govt": 40,
-            "scty": 20
-        }
-    },
-    {
-        "name": "Fundamentalism",
-        "stats": {
-            "econ": 20,
-            "dipl": 30,
-            "govt": 30,
-            "scty": 5
-        }
-    },
-    {
-        "name": "Libertarian Capitalism",
-        "stats": {
-            "econ": 20,
-            "dipl": 50,
-            "govt": 80,
-            "scty": 60
-        }
-    },
-    {
-        "name": "Market Anarchism",
-        "stats": {
-            "econ": 20,
-            "dipl": 50,
-            "govt": 100,
-            "scty": 50
-        }
-    },
-    {
-        "name": "Totalitarian Capitalism",
-        "stats": {
-            "econ": 0,
-            "dipl": 30,
+            "econ": 50,
+            "dipl": 10,
             "govt": 0,
-            "scty": 50
+            "scty": 10
         }
     },
     {
-        "name": "Ultra-Capitalism",
+        "name": "Confederación Española de Derechas",
         "stats": {
-            "econ": 0,
-            "dipl": 40,
-            "govt": 50,
-            "scty": 50
-        }
-    },
-    {
-        "name": "Anarcho-Capitalism",
-        "stats": {
-            "econ": 0,
-            "dipl": 50,
-            "govt": 100,
-            "scty": 50
+            "econ": 50,
+            "dipl": 10,
+            "govt": 10,
+            "scty": 10
         }
     }
 ];
